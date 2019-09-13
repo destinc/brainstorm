af3f3be5fd22d4625301630ee9dc1fdd5d8b7ec4
Destin Coleman <destinc@Destins-MacBook-Air.local>
2019-09-12 22:24:12 -0400

    ideas rendered on page

---
+++ client/src/components/IdeasContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class IdeasContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ideas: []
+        }
+    }
+
+    componentDidMount() {
+      axios.get('http://localhost:3001/ideas.json')
+      .then(response => {
+          console.log(response)
+          this.setState({ideas: response.data})
+      })
+      .catch(error => console.log(error))
+    }
+  render() {
+    return (
+      <div>
+        {this.state.ideas.map((idea) => {
+            return(
+                <div className="post-it" key={idea.id}>
+                    <h4>{idea.title}</h4>
+                    <p>{idea.body}</p>
+                </div>
+            )
+        })}
+      </div>
+    );
+  }
+}
+
+export default IdeasContainer
