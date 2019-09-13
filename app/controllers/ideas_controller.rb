7fcf978ce360d73b8546eeb794fb05909748e291
Destin Coleman <destinc@Destins-MacBook-Air.local>
2019-09-12 21:38:22 -0400

    front and back skeleton

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,8 @@
+
+  class IdeasController < ApplicationController
+    def index
+      @ideas = Idea.all 
+      render json: @ideas
+    end
+  end
+
---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+ideas = Idea.create(
+  [
+    {
+      title: "New App",
+      body: "Awesome App"
+    },
+    {
+      title: "A twitter client idea",
+      body: "Like twitter, but for food"
+    },
+    {
+      title: "The Martian Falcon",
+      body: "Space Detective novel"
+    },
+    {
+      title: "Card game design",
+      body: "Whist meets Pokemon"
+    }
+  ])
